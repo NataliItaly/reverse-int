51cc967a6e477e6b39b353e63557e5f73e4676dc
Krylova Nataliya <natalinebo2014@yandex.ru>
2022-01-18 12:16:47 +0100

    task reverse-int resolved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function reverse (n) {
-  
+    let str = '';
+	let strReverse = '';
+	if (typeof n !== 'number' ) {
+		
+		return;
+	}
+	else if (n >= 0) {
+		str = String(n);
+	    for (let i = str.length-1; i >= 0; i--) {
+			strReverse += str[i];
+		}
+		let reverseNum = +strReverse;
+		return reverseNum;
+    }
+	else if (n < 0) {
+		str = String(n);
+		for (let i = str.length-1; i >= 1; i--) {
+			strReverse += str[i];
+		} 
+		let reverseNum = + strReverse;
+		return reverseNum;
+	}
 }
